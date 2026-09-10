7f4b443c777d7d5d26d405665e61afc8a0444273
MattKoRn <mattkorn2@gmail.com>
2026-09-10 20:59:51 +1000

    Export QZod_DnSeparate SDL helper API on Windows

--- ZodEgine_Libs/QZod_DnSeparate/zsdl.h
+++ ZodEgine_Libs/QZod_DnSeparate/zsdl.h
@@ -59,28 +59,28 @@ private:
 	SDL_Surface *rotated_surface[360];
 };
 
-SDL_Surface *ZSDL_ConvertImage(SDL_Surface *src);
-SDL_Surface *ZSDL_IMG_Load(string filename);
-SDL_Surface *IMG_Load_Error(string filename);
-Mix_Music *MUS_Load_Error(string filename);
-Mix_Chunk *MIX_Load_Error(string filename);
-SDL_Surface *CopyImage(SDL_Surface *original);
-SDL_Surface *CopyImageShifted(SDL_Surface *original, int x, int y);
-void put32pixel(SDL_Surface *surface, int x, int y, SDL_Color color);
-SDL_Color get32pixel(SDL_Surface *surface, int x, int y);
-int ZSDL_PlayMusic(Mix_Music *music, int eh);
-int ZMix_PlayChannel(int ch, Mix_Chunk *wav, int repeat);
-void ZSDL_SetMusicOn(bool iton);
-void draw_box(SDL_Surface *surface, SDL_Rect dim, SDL_Color color, int max_x, int max_y);
-void draw_selection_box(SDL_Surface *surface, SDL_Rect dim, SDL_Color color, int max_x, int max_y);
-int AngleFromLoc(float dx, float dy);
-SDL_Surface *ZSDL_NewSurface(int w, int h);
-void ZSDL_ModifyBlack(SDL_Surface *surface);
-void ZSDL_BlitSurface(SDL_Surface *src, int fx, int fy, int fw, int fh, SDL_Surface *dest, int x, int y);
-void ZSDL_BlitHitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_Rect *dstrect, bool render_hit = true);
-void ZSDL_BlitTileSurface(SDL_Surface *src, int fx, int fy, SDL_Surface *dest, int x, int y);
-void ZSDL_FreeSurface(SDL_Surface *&surface);
-void ZSDL_Quit();
+QZOD_DNSEPARATESHARED_EXPORT SDL_Surface *ZSDL_ConvertImage(SDL_Surface *src);
+QZOD_DNSEPARATESHARED_EXPORT SDL_Surface *ZSDL_IMG_Load(string filename);
+QZOD_DNSEPARATESHARED_EXPORT SDL_Surface *IMG_Load_Error(string filename);
+QZOD_DNSEPARATESHARED_EXPORT Mix_Music *MUS_Load_Error(string filename);
+QZOD_DNSEPARATESHARED_EXPORT Mix_Chunk *MIX_Load_Error(string filename);
+QZOD_DNSEPARATESHARED_EXPORT SDL_Surface *CopyImage(SDL_Surface *original);
+QZOD_DNSEPARATESHARED_EXPORT SDL_Surface *CopyImageShifted(SDL_Surface *original, int x, int y);
+QZOD_DNSEPARATESHARED_EXPORT void put32pixel(SDL_Surface *surface, int x, int y, SDL_Color color);
+QZOD_DNSEPARATESHARED_EXPORT SDL_Color get32pixel(SDL_Surface *surface, int x, int y);
+QZOD_DNSEPARATESHARED_EXPORT int ZSDL_PlayMusic(Mix_Music *music, int eh);
+QZOD_DNSEPARATESHARED_EXPORT int ZMix_PlayChannel(int ch, Mix_Chunk *wav, int repeat);
+QZOD_DNSEPARATESHARED_EXPORT void ZSDL_SetMusicOn(bool iton);
+QZOD_DNSEPARATESHARED_EXPORT void draw_box(SDL_Surface *surface, SDL_Rect dim, SDL_Color color, int max_x, int max_y);
+QZOD_DNSEPARATESHARED_EXPORT void draw_selection_box(SDL_Surface *surface, SDL_Rect dim, SDL_Color color, int max_x, int max_y);
+QZOD_DNSEPARATESHARED_EXPORT int AngleFromLoc(float dx, float dy);
+QZOD_DNSEPARATESHARED_EXPORT SDL_Surface *ZSDL_NewSurface(int w, int h);
+QZOD_DNSEPARATESHARED_EXPORT void ZSDL_ModifyBlack(SDL_Surface *surface);
+QZOD_DNSEPARATESHARED_EXPORT void ZSDL_BlitSurface(SDL_Surface *src, int fx, int fy, int fw, int fh, SDL_Surface *dest, int x, int y);
+QZOD_DNSEPARATESHARED_EXPORT void ZSDL_BlitHitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_Rect *dstrect, bool render_hit = true);
+QZOD_DNSEPARATESHARED_EXPORT void ZSDL_BlitTileSurface(SDL_Surface *src, int fx, int fy, SDL_Surface *dest, int x, int y);
+QZOD_DNSEPARATESHARED_EXPORT void ZSDL_FreeSurface(SDL_Surface *&surface);
+QZOD_DNSEPARATESHARED_EXPORT void ZSDL_Quit();
 
 
 
